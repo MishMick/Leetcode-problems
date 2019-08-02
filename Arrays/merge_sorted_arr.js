fb9e8519fae5596b3193e9203dbe57a1094823cc
Mishal <mishallovic@gmail.com>
2019-08-03 00:59:00 +0530

    Merge sorted arr

---
+++ Arrays/merge_sorted_arr.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+ // Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+var merge = function(nums1, m, nums2, n) {
+    for(i=0;i<n;i++){
+        nums1.splice(m+i,1,nums2[i]);
+    }
+    return nums1.sort((a, b) => a - b);
+};
+
+/* Performance 
+*   Runtime: 52 ms, faster than 88.63% of JavaScript online submissions for Merge Sorted Array.
+*   Memory Usage: 35.1 MB, less than 7.92% of JavaScript online submissions for Merge Sorted Array.
+*/
