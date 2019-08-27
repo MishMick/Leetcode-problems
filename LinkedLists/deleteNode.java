5fda2eddc54d18a381e7d89fa5c6571906f1a90c
Mishal <mishallovic@gmail.com>
2019-08-27 07:20:45 +0530

    Delete node linked list

---
+++ LinkedLists/deleteNode.java
@@ -0,0 +1,13 @@
+// Write a function to delete a node (except the tail) in a singly linked list, given only access to that node
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+/* Performance :
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Delete Node in a Linked List.
+Memory Usage: 36.5 MB, less than 100.00% of Java online submissions for Delete Node in a Linked List.
+*/
